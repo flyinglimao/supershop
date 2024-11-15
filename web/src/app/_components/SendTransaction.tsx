3af4459b72912fc07c6021abe51a0a9f5f2e29f2
flyinglimao <hi@limaois.me>
2024-11-15 23:24:51 +0700

    feat: add send tx from aa

--- web/src/app/_components/SendTransaction.tsx
+++ web/src/app/_components/SendTransaction.tsx
@@ -6,12 +6,11 @@ import { createKernelAccountClient } from "@zerodev/sdk";
 import { KernelSmartAccount } from "@zerodev/sdk/accounts";
 import { KernelAccountClient } from "@zerodev/sdk/clients";
 import { EntryPoint } from "permissionless/types";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { parseEther, Transport } from "viem";
 import { type Chain as ViemChain } from "viem/chains";
 export function SendTransaction() {
   const { primaryWallet } = useDynamicContext();
-  const [q, setQ] = useState(0);
   const [kernelClient, setKernelClient] =
     useState<
       KernelAccountClient<
@@ -26,23 +25,25 @@ export function SendTransaction() {
     if (!primaryWallet) return;
     const { connector } = primaryWallet;
 
-    console.log(
-      connector,
-      isZeroDevConnector(connector),
-      isZeroDevConnector(connector) && connector.getAccountAbstractionProvider()
-    );
     if (!isZeroDevConnector(connector)) return;
 
-    // @ts-ignore
-    setKernelClient(connector.getAccountAbstractionProvider());
+    const pollingClient = setInterval(() => {
+      if (!connector.getAccountAbstractionProvider()) return;
+
+      // @ts-ignore
+      setKernelClient(connector.getAccountAbstractionProvider());
+      clearInterval(pollingClient);
+    }, 500);
+
+    return () => clearInterval(pollingClient);
   }, [primaryWallet]);
 
   if (!kernelClient) return null;
 
   return (
     <button
-      onClick={() => {
-        kernelClient.sendTransactions({
+      onClick={async () => {
+        const tx = await kernelClient.sendTransactions({
           transactions: [
             {
               to: "0xf8F7873f80039D59783e7059ECfF5A6C49D70d47",
@@ -50,12 +51,13 @@ export function SendTransaction() {
               data: "0x",
             },
             {
-              to: "0x211b6f3bd38C9D3a45cFABf9C29110A8D8911072",
-              value: parseEther("0.0011"),
-              data: "0x",
+              to: "0xf8F7873f80039D59783e7059ECfF5A6C49D70d47",
+              value: parseEther("0.002"),
+              data: "0x0012",
             },
           ],
         });
+        console.log(tx);
       }}
     >
       Send Transaction
