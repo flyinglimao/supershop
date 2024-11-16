e850ca8ccdbe8ad1ce610b636c847cd4385808a7
yihsiu <s30401s@gmail.com>
2024-11-17 01:28:32 +0700

    Initialize subgraph

---
+++ the-graph-supershop/supershop-base-sepolia/src/store-event.ts
@@ -0,0 +1,77 @@
+import {
+  OwnershipTransferred as OwnershipTransferredEvent,
+  RegisterStoreEvent as RegisterStoreEventEvent,
+  ReleaseItemEvent as ReleaseItemEventEvent,
+  RemoveItemEvent as RemoveItemEventEvent
+} from "../generated/StoreEvent/StoreEvent"
+import {
+  OwnershipTransferred,
+  RegisterStoreEvent,
+  ReleaseItemEvent,
+  RemoveItemEvent
+} from "../generated/schema"
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRegisterStoreEvent(event: RegisterStoreEventEvent): void {
+  let entity = new RegisterStoreEvent(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.store = event.params.store
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleReleaseItemEvent(event: ReleaseItemEventEvent): void {
+  let entity = new ReleaseItemEvent(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.itemId = event.params.itemId
+  entity.price = event.params.price
+  entity.store = event.params.store
+  entity.itemName = event.params.itemName
+  entity.itemDescription = event.params.itemDescription
+  entity.tags = event.params.tags
+  entity.metadata = event.params.metadata
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRemoveItemEvent(event: RemoveItemEventEvent): void {
+  let entity = new RemoveItemEvent(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.itemId = event.params.itemId
+  entity.store = event.params.store
+  entity.itemName = event.params.itemName
+  entity.itemDescription = event.params.itemDescription
+  entity.tags = event.params.tags
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
